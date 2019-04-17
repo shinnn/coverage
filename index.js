1f59cb4eddb54da44b750453780f4e2a96773351
Shinnosuke Watanabe <snnskwtnb@gmail.com>
2019-04-17 17:12:53 +0900

    use codecov-bash also on Travis CI Windows build

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 'use strict';
 
-const assert = require('assert').strict;
+const {createWriteStream, mkdir} = require('fs');
 const {dirname, join, resolve: resolvePath} = require('path');
 const {promisify} = require('util');
 const {spawn} = require('child_process');
@@ -9,6 +9,7 @@ const {spawn} = require('child_process');
 const foregroundChild = require('foreground-child');
 const normalizePackageData = require('normalize-package-data');
 const slash = require('slash');
+const uuidV4 = require('uuid/v4');
 const which = require('which');
 const yargsParser = require('yargs-parser');
 
@@ -25,6 +26,9 @@ const {_: [command], reporter} = yargsParser(optionArgs);
 const promisifiedWhich = promisify(which);
 const timeout = 2 ** 32 / 2 - 1;
 const willUploadLcov = /^1|true$/ui.test(process.env.CI) || !!process.env.GITHUB_ACTION;
+const isTravisCi = process.env.TRAVIS === 'true';
+// On Windows, write codecov-bash to a file instead of memory to avoid ENAMETOOLONG error
+const codecovBashPath = process.platform === 'win32' ? join(cwd, 'coverage', uuidV4()) : null;
 
 (async () => {
 	if (command === undefined) {
@@ -98,11 +102,10 @@ const willUploadLcov = /^1|true$/ui.test(process.env.CI) || !!process.env.GITHUB
 		})(),
 		(async () => {
 			try {
-				assert(
-					process.env.APPVEYOR !== 'true' && process.env.APPVEYOR !== 'True',
-					'Using codecov-node instead of codecov-bash to avoid Windows ENAMETOOLONG error.'
-				);
-				await Promise.all([promisifiedWhich('bash'), promisifiedWhich('git')]);
+				// The default shell of Travis CI Windows build is Git BASH
+				if (!(process.platform === 'win32' && isTravisCi)) {
+					await Promise.all([promisifiedWhich('bash'), promisifiedWhich('git')]);
+				}
 			} catch {
 				/*
 				On Node.js >= 11.3.0, const {once} = require('events') is available
@@ -130,7 +133,7 @@ const willUploadLcov = /^1|true$/ui.test(process.env.CI) || !!process.env.GITHUB
 					cwd,
 					shell: process.platform === 'win32',
 					timeout
-				}).once('exit', () => resolve('')));
+				}).once('exit', () => resolve(null)));
 			}
 
 			const {connect} = require('http2');
@@ -143,12 +146,19 @@ const willUploadLcov = /^1|true$/ui.test(process.env.CI) || !!process.env.GITHUB
 			request.setEncoding('utf8');
 			request.end();
 
-			await promisify(pipeline)(request, new Writable({
-				write(chunk, _, cb) {
-					body += chunk;
-					cb();
-				}
-			}));
+			if (codecovBashPath) {
+				await promisify(mkdir)(dirname(codecovBashPath), {recursive: true});
+			}
+
+			await promisify(pipeline)([
+				request,
+				codecovBashPath ? createWriteStream(codecovBashPath) : new Writable({
+					write(chunk, _, cb) {
+						body += chunk;
+						cb();
+					}
+				})
+			]);
 
 			return body;
 		})()
@@ -158,18 +168,14 @@ const willUploadLcov = /^1|true$/ui.test(process.env.CI) || !!process.env.GITHUB
 		process.exit(code);
 	}
 
-	const isTravisCi = process.env.TRAVIS === 'true';
-
 	if (isTravisCi) {
 		console.log('travis_fold:start:codecov\nupload coverage to codecov.io');
 	}
 
-	const reportArgs = codecovBash ? [
+	const reportArgs = codecovBash !== null ? [
 		'bash',
 		[
-			'-c',
-			codecovBash,
-			'--',
+			...codecovBashPath ? [codecovBashPath] : ['-c', codecovBash, '--'],
 			'-X',
 			'gcov',
 			'-X',
