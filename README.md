75950586dad9b0952af610981a9d5a5a81f85280
Shinnosuke Watanabe <snnskwtnb@gmail.com>
2019-05-07 20:16:23 +0900

    sdf

--- README.md
+++ README.md
@@ -29,8 +29,9 @@ All files |    95.15 |    79.52 |      100 |    95.09 |                   |
 
 This is an opinionated wrapper of [c8](https://github.com/bcoe/c8), different in the following points:
 
+* Supports multiple file execution `coverage test.js test-2.js`
 * Runs both [`html` and `text` reporter](https://github.com/istanbuljs/nyc#running-reports) by default
-* Automatically enables [ECMAScript module](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/import) support for `.mjs` files
+* Automatically executes `.mjs` files as [ECMAScript modules](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/import)
 * The first argument can be a JavaScript file path instead of a command
 * Built-in [Codecov](https://codecov.io) support
 
--- index.js
+++ index.js
@@ -3,9 +3,10 @@
 
 const {createWriteStream, mkdir} = require('fs');
 const {dirname, extname, join, resolve: resolvePath} = require('path');
+const {execFileSync, spawn} = require('child_process');
 const {promisify} = require('util');
-const {spawn} = require('child_process');
 
+const {defaultExclude} = require('test-exclude');
 const foregroundChild = require('foreground-child');
 const normalizePackageData = require('normalize-package-data');
 const slash = require('slash');
@@ -19,15 +20,36 @@ const c8PackageData = require(c8PackageJsonPath);
 normalizePackageData(c8PackageData);
 
 const c8BinPath = join(dirname(c8PackageJsonPath), c8PackageData.bin.c8);
-const nodePath = process.execPath;
 const optionArgs = process.argv.slice(2);
-const {_: [command], reporter} = yargsParser(optionArgs);
+const {_: positionalArgs, reporter} = yargsParser(optionArgs);
+const [command, ...restArgs] = positionalArgs;
 const promisifiedWhich = promisify(which);
 const timeout = 2 ** 32 / 2 - 1;
 const willUploadLcov = /^1|true$/ui.test(process.env.CI) || !!process.env.GITHUB_ACTION;
 const isTravisCi = process.env.TRAVIS === 'true';
 // On Windows, write codecov-bash to a file instead of memory to avoid ENAMETOOLONG error
 const codecovBashPath = process.platform === 'win32' ? join(cwd, 'coverage', Math.random().toString()) : null;
+const esModulesArgs = [
+	'--experimental-modules',
+	'--es-module-specifier-resolution=node',
+	'--experimental-json-modules',
+	'--no-warnings'
+];
+const c8Args = [
+	// Remove this when https://github.com/istanbuljs/istanbuljs/pull/381 is published
+	...defaultExclude.map(pattern => `--exclude="${pattern.replace(/(?<=\.)(?=js$)/ui, '{,c,m}')}"`),
+	'--exclude="**/*.json"'
+];
+const c8ReporterArgs = reporter === undefined ? [
+	'--reporter=text',
+	`--reporter=${willUploadLcov ? 'lcovonly' : 'html'}`
+] : [];
+const childProcessOptions = {
+	cwd,
+	stdio: 'inherit',
+	timeout
+};
+let exitCode;
 
 (async () => {
 	if (command === undefined) {
@@ -36,7 +58,7 @@ const codecovBashPath = process.platform === 'win32' ? join(cwd, 'coverage', Mat
 	}
 
 	if (command === 'report') {
-		foregroundChild(nodePath, [c8BinPath, ...optionArgs]);
+		foregroundChild(process.execPath, [c8BinPath, ...optionArgs]);
 		return;
 	}
 
@@ -61,69 +83,84 @@ const codecovBashPath = process.platform === 'win32' ? join(cwd, 'coverage', Mat
 				entryPath = require.resolve(entryPath);
 			} catch {
 				console.error(`Both a command \`${command}\` and a Node.js entry point ${entryPath} don't exist.`);
-				process.exit(127);
+				process.exitCode = 127;
+
+				return [];
 			}
 
-			optionArgs.splice(
-				optionArgs.indexOf(command),
-				1,
-				nodePath,
-				...ext === 'mjs' ? [
-					'--experimental-modules',
-					'--es-module-specifier-resolution=node',
-					'--experimental-json-modules',
-					'--no-warnings'
-				] : [],
-				entryPath
-			);
+			if (restArgs.length === 0) {
+				optionArgs.splice(
+					optionArgs.indexOf(command),
+					1,
+					process.execPath,
+					...ext === 'mjs' ? esModulesArgs : [],
+					entryPath
+				);
+
+				return [[c8BinPath, ...c8Args, ...c8ReporterArgs, ...optionArgs]];
+			}
+
+			for (const positionalArg of positionalArgs) {
+				optionArgs.splice(optionArgs.indexOf(positionalArg), 1);
+			}
+
+			const results = positionalArgs.map((positionalArg, index) => {
+				let restEntryPath = resolvePath(cwd, positionalArg);
+
+				try {
+					restEntryPath = require.resolve(entryPath);
+				} catch {
+					console.error(`A Node.js entry point ${restEntryPath} does't exist.`);
+					process.exitCode = 127;
+				}
+
+				return [
+					c8BinPath,
+					...c8Args,
+					...index === positionalArgs.length - 1 ? c8ReporterArgs : ['--reporter=none'],
+					...optionArgs,
+					...index !== 0 ? ['--clean=false'] : [],
+					process.execPath,
+					...extname(positionalArg).toLowerCase() === '.mjs' ? esModulesArgs : [],
+					restEntryPath
+				];
+			});
+
+			return results;
 		}
 
-		return [
-			nodePath,
-			[
-				c8BinPath,
-				// Remove this when https://github.com/istanbuljs/istanbuljs/pull/381 is merged
-				...require('test-exclude').defaultExclude.map(pattern => `--exclude="${pattern.replace(/(?<=\.)(?=js$)/ui, '{,c,m}')}"`),
-				'--exclude="**/*.json"',
-				...reporter === undefined ? [
-					'--reporter=text',
-					`--reporter=${willUploadLcov ? 'lcovonly' : 'html'}`
-				] : [],
-				...optionArgs
-			]
-		];
+		return [[c8BinPath, ...c8Args, ...optionArgs]];
 	})();
 
 	if (!willUploadLcov) {
-		foregroundChild(...await prepareArgs);
+		const preparedArgs = await prepareArgs;
+
+		if (process.exitCode) {
+			return;
+		}
+
+		const foregroundArgs = preparedArgs.pop();
+
+		for (const args of preparedArgs) {
+			try {
+				execFileSync(process.execPath, args, childProcessOptions);
+			} catch ({status}) {
+				exitCode = status;
+			}
+		}
+
+		foregroundChild(process.execPath, foregroundArgs, done => {
+			if (exitCode !== undefined) {
+				process.exitCode = exitCode;
+			}
+
+			done();
+		});
+
 		return;
 	}
 
-	const [code, codecovBash] = await Promise.all([
-		(async () => {
-			/*
-			On Node.js >= 11.3.0, const {once} = require('events') is available
-			https://nodejs.org/api/events.html#events_events_once_emitter_name
-
-			return (await once(spawn(...await prepareArgs, {
-				cwd,
-				stdio: 'inherit',
-				timeout
-			}), 'exit'))[0];
-			*/
-
-			const args = await prepareArgs;
-
-			return new Promise((resolve, reject) => {
-				spawn(...args, {
-					cwd,
-					stdio: 'inherit',
-					timeout
-				})
-				.once('error', reject)
-				.once('exit', resolve);
-			});
-		})(),
+	const [codecovBash] = await Promise.all([
 		(async () => {
 			try {
 				// The default shell of Travis CI Windows build is Git BASH
@@ -185,11 +222,33 @@ const codecovBashPath = process.platform === 'win32' ? join(cwd, 'coverage', Mat
 			]);
 
 			return body;
+		})(),
+		(async () => {
+			/*
+			On Node.js >= 11.3.0, const {once} = require('events') is available
+			https://nodejs.org/api/events.html#events_events_once_emitter_name
+
+			return (await once(spawn(process.execPath, (await prepareArgs)[0], childProcessOptions), 'exit'))[0];
+			*/
+
+			const onExit = code => {
+				if (code) {
+					process.exitCode = code;
+				}
+			};
+
+			for (const args of await prepareArgs) {
+				await new Promise((resolve, reject) => { // eslint-disable-line no-await-in-loop
+					spawn(process.execPath, args, childProcessOptions)
+					.once('error', reject)
+					.once('exit', onExit);
+				});
+			}
 		})()
 	]);
 
-	if (code !== 0) {
-		process.exit(code);
+	if (process.exitCode) {
+		return;
 	}
 
 	if (isTravisCi) {
